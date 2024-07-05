32d8aa082f699226cb6f5d3cffb7dd4d344b8a84
Ubaid Qureshi <159818516+parween47@users.noreply.github.com>
2024-07-05 22:45:23 +0500

    uploading task 20
    uploading  task 20

---
+++ task20.ts
@@ -0,0 +1,59 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement
+// describing each test and your prediction for the results of each test. Your code
+// should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+let person:string="Furqan"
+let height:number=7
+let bankBalance:number=100000
+
+console.log("Is person == 'furqan'? I predict true.");
+console.log(person == "Furqan");
+
+console.log("Is height == 7? I predict true.");
+console.log(height == 7);
+
+console.log("Is bankBalance == 100000? I predict true.");
+console.log(bankBalance == 100000);
+
+console.log("Is height > 5? I predict true.");
+console.log(height > 5);
+
+console.log("Is bankBalance < 200000? I predict true.");
+console.log( bankBalance < 200000);
+
+console.log("Is person === 'furqan'? I predict false.");
+console.log(person === "furqan");
+
+console.log("Is height !== 7? I predict false.");
+console.log(height !== 7);
+
+console.log("Is bankBalance == 100000 && height === 8? I predict false.");
+console.log(bankBalance == 100000 && height === 8);
+
+console.log("Is height < 5? I predict false.");
+console.log(height < 5);
+ 
+console.log("Is bankBalance >= 200000? I predict false.");
+console.log( bankBalance >= 200000);
+
+
+
+
+
+
+
+
+
+
+
+
